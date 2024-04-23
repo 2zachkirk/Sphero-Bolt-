3930caf2618677e28aa86175decc157c92bf2b33
Kirk <2zach.kirk@gulllakecs.org>
2024-04-23 10:36:04 -0700

    Initial MyMiniCourse.js part 2 commit

---
+++ MyMiniCourse.js
@@ -0,0 +1,10 @@
+// Programmer: Zach Kirk 
+// Team Member: Marcus Campos-Jones 
+// Bolt ID: 2FAE
+// Date 4.23.24: 
+// Program: My Mini Course
+// Program URL: https://edu.sphero.com/program/16743347/edit
+
+async function startProgram() {
+	// Write code here
+}
