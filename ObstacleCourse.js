fabf3bfcbe5cbbefb49e889adc090b0654011cec
Kirk <2zach.kirk@gulllakecs.org>
2024-04-24 11:15:09 -0700

    Obstacle Part 2 commit - Success

---
+++ ObstacleCourse.js
@@ -0,0 +1,30 @@
+// Programmer: Zach Kirk 
+// Team Member: Marcus Campos-Jones 
+// Bolt ID: 2FAE
+// Date 4.23.24: 
+// Program: Obstacle Course 
+// Program URL: https://edu.sphero.com/program/16743347/edit
+
+//Player 1. Zach Kirk
+//Player 2. Marcus Campos-Jones 
+/*
+//checkpoint one
+async function startProgram() {
+	setMainLed({ r: 200, g:55, b: 0 });
+	await roll((getHeading() + 0), 60, 3);	
+	await delay(1);
+	await roll((getHeading() + 270), 40, 2);
+	await delay(1);
+	await roll((getHeading() + 270), 50, 2);
+}
+*/
+//player 1 code Marcus Campos-Jones
+
+
+// checkpoint two
+async function startProgram() {
+	await roll((getHeading() + 90), 40, 2);
+	await delay(1);
+	await roll((getHeading() + 90), 60, 2);
+	
+}
