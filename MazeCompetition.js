1ce1ad207b6cded7c4b56c9bcfde9710e0bb12b1
Kirk <2zach.kirk@gulllakecs.org>
2024-04-30 11:18:40 -0700

    Programmer: Zach Kirk- Start Text & audio Checkpoint Commit

--- MazeCompetition.js
+++ MazeCompetition.js
@@ -6,5 +6,35 @@
 // Program URL: https://edu.sphero.com/program/16788339/edit
 
 async function startProgram() {
-	// Write code here
+	// Programmer: Zach- Start Text & audio Checkpoint Commit - Date: 
+	await speak("start",true)
+	await scrollMatrixText('start!', { r: 66, g: 56, b: 255 }, 30, true)
+	
+	// Programmer:        - Blue LED Checkpoint Commit - Date: 
+
+
+
+    // Programmer:        - Sound One Checkpoint Commit - Date: 
+
+
+ 
+    // Programmer:        - Red LED Checkpoint Commit - Date: 
+
+
+
+    // Programmer:        - Sound Two Checkpoint Commit - Date: 
+
+
+  
+    // Programmer:        - Green LED Checkpoint Commit - Date: 
+
+
+
+    // Programmer:        - Purple LED & Sound Three Checkpoint Commit - Date: 
+
+
+
+    // Programmer:        - Finish Text & Audio Checkpoint Commit - Date: 
+
+
 }
