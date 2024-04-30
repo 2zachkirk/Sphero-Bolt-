ccf5d291057b8ebb8d8d6ac5786ed50b1c336f42
Kirk <2zach.kirk@gulllakecs.org>
2024-04-30 10:28:29 -0700

    Added Beginning Comments and "Start" program JS

---
+++ MazeCompetition.js
@@ -0,0 +1,10 @@
+// Programmer: Zach Kirk 
+// Team Member: Marcus Campos-Jones 
+// Bolt ID: 2FAE
+// Date 4.23.24: 
+// Program: Maze Competition  
+// Program URL: https://edu.sphero.com/program/16788339/edit
+
+async function startProgram() {
+	// Write code here
+}
